07de6208d8949d8836a9185fa67c050bdfd9ba6f
iannika <iannika7@gmail.com>
2023-10-05 10:29:11 +0300

    adding files

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: '--language=es' or '--language=ru'")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    print("\nstart chrome browser with {} language..".format(language))
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    # time.sleep(30)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
